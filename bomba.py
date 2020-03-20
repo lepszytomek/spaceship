46df99481edccfc66b67fbdd9876c4ed7d78cd0f
Ola <olapyt@gmail.com>
2020-03-20 10:52:16 +0100

    bomba zabija

--- bomba.py
+++ bomba.py
@@ -5,9 +5,10 @@ class Bomba(object):
 
     def __init__(self, game):
         self.game = game
-        self.czas_do_wybuch = 500.0
+        self.poczantkowy_czas_wybuchu = 250.0
+        self.czas_do_wybuch = self.poczantkowy_czas_wybuchu
         self.pos = Vector2()
-        self.obszar_wybuchu = 50
+        self.obszar_wybuchu = 100
         self.jest_bomba = False
 
     def stawianie_bomby(self):
@@ -18,9 +19,13 @@ class Bomba(object):
     def tick(self):
             if self.jest_bomba:
                 if self.czas_do_wybuch <= 0:
-
-                    if self.pos == self.game.player.pos:
-                        print('bomb kill you')
+                    self.jest_bomba = False
+                    self.czas_do_wybuch = self.poczantkowy_czas_wybuchu
+                    pos = self.game.player.pos - self.pos
+                    pos.x = abs(pos.x)
+                    pos.y = abs(pos.y)
+                    if pos.x < self.obszar_wybuchu and pos.y < self.obszar_wybuchu:
+                        print("bomb kill you")
                         sys.exit(0)
                 else:
                     self.czas_do_wybuch -= 1
@@ -33,5 +38,4 @@ class Bomba(object):
     def handlet_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             self.stawianie_bomby()
-        pass
 
